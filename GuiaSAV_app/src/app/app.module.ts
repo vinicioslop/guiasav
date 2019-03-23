f4c76e3b548c874a5a6836943871026953940bf1
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-03-23 01:12:39 -0300

    import
    adicionando impportes ao app.module

--- GuiaSAV_app/src/app/app.module.ts
+++ GuiaSAV_app/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AnatomiaPageModule } from '../pages/anatomia/anatomia.module';
+import { AbreviacaoPageModule } from '../pages/abreviacao/abreviacao.module';
+import { ContencaoFisicaPageModule } from '../pages/contencao-fisica/contencao-fisica.module';
+import { DicionarioTermoPageModule } from '../pages/dicionario-termo/dicionario-termo.module';
+import { EquipamentoMonitorizacaoPageModule } from '../pages/equipamento-monitorizacao/equipamento-monitorizacao.module';
+import { ExameFisicoGeralPageModule } from '../pages/exame-fisico-geral/exame-fisico-geral.module';
+import { FarmacologiaPageModule } from '../pages/farmacologia/farmacologia.module';
+import { InstrumentoCirurgicoPageModule } from '../pages/instrumento-cirurgico/instrumento-cirurgico.module';
+import { FormulaPageModule } from '../pages/formula/formula.module';
+import { PrecaucaoPadraoPageModule } from '../pages/precaucao-padrao/precaucao-padrao.module';
+import { VerificacaoSinalVitalPageModule } from '../pages/verificacao-sinal-vital/verificacao-sinal-vital.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +25,19 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    //menu principal
+    AnatomiaPageModule,
+    AbreviacaoPageModule,
+    ContencaoFisicaPageModule,
+    DicionarioTermoPageModule,
+    EquipamentoMonitorizacaoPageModule,
+    ExameFisicoGeralPageModule,
+    FarmacologiaPageModule,
+    FormulaPageModule,
+    InstrumentoCirurgicoPageModule,
+    PrecaucaoPadraoPageModule,
+    VerificacaoSinalVitalPageModule,
+
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
