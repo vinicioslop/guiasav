f7a4b0cd5aab58a71bbe1725cb343c8314c44b38
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-03-23 00:38:44 -0300

    Paginas
    metade das paginas do app

---
+++ GuiaSAV_app/src/pages/anatomia/anatomia.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AnatomiaPage } from './anatomia';
+
+@NgModule({
+  declarations: [
+    AnatomiaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AnatomiaPage),
+  ],
+})
+export class AnatomiaPageModule {}
