72b9b18b45587b822f95930936ae4b8e040eff70
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-03-23 15:17:55 -0300

    Fim da par2
    todos os importes funcionando e links direcionados.

--- GuiaSAV_app/src/pages/precaucao-padrao/precaucao-padrao.ts
+++ GuiaSAV_app/src/pages/precaucao-padrao/precaucao-padrao.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HigieneMaoPage } from '../higiene-mao/higiene-mao';
+import { MascaraProtetorOcularPage } from '../mascara-protetor-ocular/mascara-protetor-ocular';
+import { AventalPage } from '../avental/avental';
+import { ControleAmbientePage } from '../controle-ambiente/controle-ambiente';
+import { MaterialPerfuroCortantePage } from '../material-perfuro-cortante/material-perfuro-cortante';
+import { ManuseioLuvaEsterilPage } from '../manuseio-luva-esteril/manuseio-luva-esteril';
 
 /**
  * Generated class for the PrecaucaoPadraoPage page.
@@ -21,5 +27,22 @@ export class PrecaucaoPadraoPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad PrecaucaoPadraoPage');
   }
-
+  hig_maoClick(){
+    this.navCtrl.push(HigieneMaoPage);
+  }
+  mas_prot_ocularClick(){
+    this.navCtrl.push(MascaraProtetorOcularPage);
+  }
+  aventalClick(){
+    this.navCtrl.push(AventalPage);
+  }
+  con_ambienteClick(){
+    this.navCtrl.push(ControleAmbientePage);
+  }
+  mat_perf_cortanteClick(){
+    this.navCtrl.push(MaterialPerfuroCortantePage);
+  }
+  manuseio_luvas_esterilClick(){
+    this.navCtrl.push(ManuseioLuvaEsterilPage);
+  }
 }
