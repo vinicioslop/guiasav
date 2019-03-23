4f14854128c055a2d0a894eabc98150d4749cd06
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-03-23 02:12:08 -0300

    Erro
    Unexpected directive 'AnatomiaPage' imported by the module 'AppModule'. Please add a @NgModule annotation. Mas quando adiciono no  @NgModule o blablablaPage não funciona

--- GuiaSAV_app/src/pages/home/home.ts
+++ GuiaSAV_app/src/pages/home/home.ts
@@ -1,16 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AbreviacaoPage } from '../abreviacao/abreviacao';
 import { AnatomiaPage } from '../anatomia/anatomia';
-import { ContencaoFisicaPage } from '../contencao-fisica/contencao-fisica';
-import { DicionarioTermoPage } from '../dicionario-termo/dicionario-termo';
-import { EquipamentoMonitorizacaoPage } from '../equipamento-monitorizacao/equipamento-monitorizacao';
-import { ExameFisicoGeralPage } from '../exame-fisico-geral/exame-fisico-geral';
-import { FarmacologiaPage } from '../farmacologia/farmacologia';
-import { FormulaPage } from '../formula/formula';
-import { InstrumentoCirurgicoPage } from '../instrumento-cirurgico/instrumento-cirurgico';
-import { PrecaucaoPadraoPage } from '../precaucao-padrao/precaucao-padrao';
-import { VerificacaoSinalVitalPage } from '../verificacao-sinal-vital/verificacao-sinal-vital';
 
 @Component({
   selector: 'page-home',
@@ -24,34 +14,4 @@ export class HomePage {
   anatomiaClick(){
     this.navCtrl.push(AnatomiaPage);
   }
-  abreviacaoClick(){
-   this.navCtrl.push(AbreviacaoPage);
- }
- cont_fisicoClick(){
-   this.navCtrl.push(ContencaoFisicaPage);
- }
- dic_termoClick(){
-   this.navCtrl.push(DicionarioTermoPage);
- }
- eq_monitorizacaoClick(){
-   this.navCtrl.push(EquipamentoMonitorizacaoPage);
- }
- ex_fisicoClick(){
-   this.navCtrl.push(ExameFisicoGeralPage);
- }
- farmacologiaClick(){
-   this.navCtrl.push(FarmacologiaPage);
- }
- formulaClick(){
-   this.navCtrl.push(FormulaPage);
- }
- ins_cirurgicoClick(){
-   this.navCtrl.push(InstrumentoCirurgicoPage);
- }
- precaucao_padraoClick(){
-   this.navCtrl.push(PrecaucaoPadraoPage);
- }
- ver_sinais_vitaisClick(){
-   this.navCtrl.push(VerificacaoSinalVitalPage);
- }
 }
