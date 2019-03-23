879374d451fab5960bc981a08694d5e9fb5004ac
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-03-23 01:18:04 -0300

    Ação botoes
    Redireciona para a pagina com a ação click.

--- GuiaSAV_app/src/pages/home/home.ts
+++ GuiaSAV_app/src/pages/home/home.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AbreviacaoPage } from '../abreviacao/abreviacao';
+import { AnatomiaPage } from '../anatomia/anatomia';
+import { ContencaoFisicaPage } from '../contencao-fisica/contencao-fisica';
+import { DicionarioTermoPage } from '../dicionario-termo/dicionario-termo';
+import { EquipamentoMonitorizacaoPage } from '../equipamento-monitorizacao/equipamento-monitorizacao';
+import { ExameFisicoGeralPage } from '../exame-fisico-geral/exame-fisico-geral';
+import { FarmacologiaPage } from '../farmacologia/farmacologia';
+import { FormulaPage } from '../formula/formula';
+import { InstrumentoCirurgicoPage } from '../instrumento-cirurgico/instrumento-cirurgico';
+import { PrecaucaoPadraoPage } from '../precaucao-padrao/precaucao-padrao';
+import { VerificacaoSinalVitalPage } from '../verificacao-sinal-vital/verificacao-sinal-vital';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +21,37 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  anatomiaClick(){
+    this.navCtrl.push(AnatomiaPage);
+  }
+  abreviacaoClick(){
+   this.navCtrl.push(AbreviacaoPage);
+ }
+ cont_fisicoClick(){
+   this.navCtrl.push(ContencaoFisicaPage);
+ }
+ dic_termoClick(){
+   this.navCtrl.push(DicionarioTermoPage);
+ }
+ eq_monitorizacaoClick(){
+   this.navCtrl.push(EquipamentoMonitorizacaoPage);
+ }
+ ex_fisicoClick(){
+   this.navCtrl.push(ExameFisicoGeralPage);
+ }
+ farmacologiaClick(){
+   this.navCtrl.push(FarmacologiaPage);
+ }
+ formulaClick(){
+   this.navCtrl.push(FormulaPage);
+ }
+ ins_cirurgicoClick(){
+   this.navCtrl.push(InstrumentoCirurgicoPage);
+ }
+ precaucao_padraoClick(){
+   this.navCtrl.push(PrecaucaoPadraoPage);
+ }
+ ver_sinais_vitaisClick(){
+   this.navCtrl.push(VerificacaoSinalVitalPage);
+ }
 }
