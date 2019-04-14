d5fb2f47a45e790f42d4981a02edd601b4da956f
Vinícius Lopes Lima <vinicioslop7@hotmail.com>
2019-04-14 18:03:52 -0300

    Auto stash before merge of "master" and "origin/master"

--- GuiaSAV_app/src/pages/home/home.ts
+++ GuiaSAV_app/src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, App } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, App, Slides } from 'ionic-angular';
 
 import { AuthService } from '../../providers/auth-service/auth-service';
 import { LoginPage } from '../login/login';
@@ -9,6 +9,20 @@ import { LoginPage } from '../login/login';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  /*@ViewChild(Slides) slides: Slides;
+
+  imageContainer = [
+  {name: 'mountain', url:"http://www.mobile-phone.pk/images/wallpapers/mountain_wallpaper_nature_mobile_wallpaper.jpg" },
+  {name : 'sky', url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMeW0Tp1XQXgMJiJcCgAEOzlibTVLJGP6XgyBTC2oOWbTM__s-"} ];
+
+  ionViewDidLoad(){
+    this.slides.paginationClickable=true;
+    this.slides.initialSlide=0;
+    this.slides.autoplay = 500;
+    this.slides.loop = true;
+    this.slides.speed = 2200;
+  }*/
 
   constructor(private navCtrl: NavController, public appCtrl: App, private auth: AuthService) {
 
@@ -25,4 +39,12 @@ export class HomePage {
       this.appCtrl.getRootNav().setRoot(LoginPage)
     });
   }
+
+  /*slideNext(){
+    this.slides.slideNext();
+  }
+
+  slidePrev(){
+    this.slides.slidePrev();
+  }*/
 }
