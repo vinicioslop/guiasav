3f51b062688662f881a60ccabcd2f77173d4ac0f
Vinícius Lopes Lima <vinicioslop7@hotmail.com>
2019-04-28 19:47:03 -0300

    Implementando api
    Atualmente com problemas de acesso a api

--- GuiaSAV_app/src/providers/api/api.ts
+++ GuiaSAV_app/src/providers/api/api.ts
@@ -4,28 +4,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ApiProvider {
 
-  private apiSAV = 'http://guiasav.diforg.com.br/ws/lista_categoria/'
+  private apiSAV_categoria = 'http://guiasav.diforg.com.br/ws/lista_categoria/'
+  private apiSAV_topico = 'http://guiasav.diforg.com.br/ws/lista_categoria/'
+  private apiSAV_conteudo = 'http://guiasav.diforg.com.br/ws/lista_categoria/'
 
   constructor(public http: Http) {
     console.log('Hello ApiProvider Provider');
   }
 
   listaCategorias() {
-    return this.http.get(this.apiSAV)
+    return this.http.get(this.apiSAV_categoria)
                     .map((data) => {
                       return data.json();
                     });              
   }
 
   listaTopicos() {
-    return this.http.get(this.apiSAV)
+    return this.http.get(this.apiSAV_topico)
                     .map((data) => {
                       return data.json();
                     });              
   }
 
   listaConteudo() {
-    return this.http.get(this.apiSAV)
+    return this.http.get(this.apiSAV_conteudo)
                     .map((data) => {
                       return data.json();
                     });              
