33a52aaa6c3b56855c0118579274f1678899da18
Vinícius Lopes Lima <vinicioslop7@hotmail.com>
2019-08-02 09:25:41 -0300

    Pequenas modificações

--- guiasav_app/src/components/Lista.js
+++ guiasav_app/src/components/Lista.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, StyleSheet, Button, FlatList, Text, TouchableHighlight, ScrollView } from 'react-native';
+import { View, Button, FlatList, ScrollView } from 'react-native';
 
 const uri = 'http://guiasav.diforg.com.br/ws';
 
@@ -9,14 +9,11 @@ export default class Lista extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            categorias: [],
-            carregado: 'false',
-            topicos: [],
-            topic: 'false'
+            categorias: []
         }
     }
 
-    carregaDados() {
+    componentDidMount(){
         fetch(uri + '/lista_categoria')
             .then(resposta => resposta.json())
             .then(json => this.setState({ categorias: json, carregado: 'true' }));
@@ -24,70 +21,31 @@ export default class Lista extends Component {
 
     listaTopico(id) {
         fetch('http://guiasav.diforg.com.br/ws/lista_topico/' + id)
-        .then(resposta => resposta.json())
-        .then(json => this.setState({ topicos: json, topic: 'true' }));
+            .then(resposta => resposta.json())
+            .then(json => this.setState({ topicos: json, topic: 'true' }));
     }
-
-
 
     render() {
         return (
             <View>
 
-                {
-                    this.state.carregado === 'true' && this.state.topic === 'false' ? null :
-                    <Button
-                        onPress={this.carregaDados.bind(this)}
-                        title={"Carrega Dados"}
-                        color={"#841584"}
-                        accessibilityLabel={"Carregamento de Dados"}
-                    />
-                }
-
-                {
-                    this.state.carregado === 'false' && this.state.topic === 'true' ? null :
-                        <FlatList
-                            data={this.state.categorias}
-                            keyExtractor={item => item.cd_category}
-                            renderItem={({ item }) =>
-                                <ScrollView>
-                                    <TouchableHighlight onPress={this.listaTopico(item.cd_category)} style={styles.categorias}>
-                                        <Text style={styles.categoriaItem}>{item.nm_category}</Text>
-                                    </TouchableHighlight>
-                                </ScrollView>
-                            }
-                        />
-                }
-
-                {
-                    /*
-                    this.state.topic === 'false' ? null :
-                    <FlatList
-                        data={this.state.topicos}
-                        keyExtractor={item => item.cd_topic}
-                        renderItem={({ item }) =>
-                            <ScrollView>
-                                <TouchableHighlight onPress={this.listaTopico(item.cd_category)} style={styles.categorias}>
-                                    <Text style={styles.categoriaItem}>{item.nm_topic}</Text>
-                                </TouchableHighlight>
-                            </ScrollView>
-                        }
-                    />
-                    */
-                }
+                <FlatList
+                    data={this.state.categorias}
+                    keyExtractor={item => item.cd_category}
+                    renderItem={({ item }) =>
+                        <ScrollView>
+                            <Button
+                                onPress={this.listaTopico(item.cd_category)}
+                                title={item.nm_category}
+                                color={"#841584"}
+                                accessibilityLabel={item.nm_category}
+                            />
+                        </ScrollView>
+                    }
+                />
 
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    categorias: {
-        alignItems: 'center'
-    },
-    categoriaItem: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        
-    }
-});
