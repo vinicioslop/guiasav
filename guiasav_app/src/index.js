e43e3fabb351e288299fdc49582dc3a9fceabe50
Vinícius Lopes Lima <vinicioslop7@hotmail.com>
2019-09-01 16:49:30 -0300

    Implementando navegação entre páginas de autentiicação
    Faltando pequenas alterações

--- guiasav_app/src/index.js
+++ guiasav_app/src/index.js
@@ -1,39 +1,62 @@
-// src/index.js
-
 import { createAppContainer, createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
-import Inicio from './screens/Inicio';
-import Categorias from './screens/conteudo/Categorias';
-import Forum from './screens/forum/Forum';
-import Perfil from './screens/perfil/Perfil';
+//Import AuthStack
+import Login from './screens/auth/Login';
+import Sign from './screens/auth/Sign';
 
+//Import Stack Inicio
+import Inicio from './screens/Inicio';
+
+//Import Stack Categorias
+import Categorias from './screens/conteudo/Categorias';
 import Topicos from './screens/conteudo/Topicos';
 import Conteudo from './screens/conteudo/Conteudo';
 
+//Import Stack Forum
+import Forum from './screens/forum/Forum';
+
+//Import Stack Perfil
+import Perfil from './screens/perfil/Perfil';
+
+
+//Definição do Stack Inicio
 const HomeStack = createStackNavigator({
-  Home: Inicio
+  Inicio: Inicio
 });
 
+//Definição do Stack Categorias
 const CategoriasStack = createStackNavigator({
   Categorias: Categorias,
   Topicos: Topicos,
   Conteudo: Conteudo
 });
 
+//Definição do Stack Forum
 const ForumStack = createStackNavigator({
   Forum: Forum
 });
 
+//Definição do Stack Perfil
 const PerfilStack = createStackNavigator({
   Perfil: Perfil
 });
 
+//Definição do TabStack
+const TabStack = createBottomTabNavigator({
+  Inicio: HomeStack,
+  Categorias: CategoriasStack,
+  Forum: ForumStack,
+  Perfil: PerfilStack
+});
+
 const Routes = createAppContainer(
-  createBottomTabNavigator({
-    Home: HomeStack,
-    Categorias: CategoriasStack,
-    Forum: ForumStack,
-    Perfil: PerfilStack
+  createStackNavigator({
+    Login: Login,
+    Sign: Sign,
+    Tab: {
+      screen: TabStack,
+      header: null
+    }
   })
 );
 
--- guiasav_app/src/screens/Inicio.js
+++ guiasav_app/src/screens/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { HeaderBackButton } from 'react-navigation';
 
 export default class Inicio extends React.Component {
 
@@ -12,7 +13,7 @@ export default class Inicio extends React.Component {
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home</Text>
         <Button
-          onPress={() => this.goToLogin}
+          onPress={() => this.goToLogin()}
           title="Login"
           color="#841584"
           accessibilityLabel="Vai pro Login"
